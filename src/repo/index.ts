d57d7d52693543ce7aa119bb2f673b72d5b2a284
TAPIOKA1113 <manato20021113@gmail.com>
2024-12-25 17:53:01 +0900

    環境変数の更新

--- src/repo/index.ts
+++ src/repo/index.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = false
 
 const supabaseUrl = isProd
-  ? process.env.SUPABASE_URL!
+  ? 'https://fnozrjnxuszijdyqzenb.supabase.co'
   : 'http://localhost:54321'
 
 const supabaseKey = isProd
-  ? process.env.SUPABASE_ANON_KEY!
+  ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZub3pyam54dXN6aWpkeXF6ZW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMTEzODgsImV4cCI6MjA1MDU4NzM4OH0.KBiXx3WLcAYiDCgyjS3r3No-BQQTUuwUd3wUgZJ6qtY'
   : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
